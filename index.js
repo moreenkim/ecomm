9d1fe6fcbc6b8d12bceb8b3a41e99448a224ee7e
moreenkim <moreenkim36@gmail.com>
2020-06-16 14:08:58 +0300

    Display user information input form

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there')
+    res.send(`
+    <div>
+     <form>
+      <input placeholder="email"/>
+      <input placeholder="Password"/>
+      <input placeholder="Password confirmation"/>
+      <button>Sign up</button>
+     </form>
+    </div>
+    `);
 });
 
 app.listen(3000, () => {

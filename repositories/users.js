e7fc1e7b871cb87ed12e5347ade106bc23e6776d
moreenkim <moreenkim36@gmail.com>
2020-06-26 11:49:10 +0300

    Add compare hashed passwords logic

--- repositories/users.js
+++ repositories/users.js
@@ -44,6 +44,16 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    // const result = saved.split('.');
+    // const hashed = result[0];
+    // const salt = result[1];
+    const [hashed, salt] = saved.split('.');
+    const hashedSupplied = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSupplied;
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(
       this.filename,

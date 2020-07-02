adbff224aec2321c35e13a492bfb72879f888399
moreenkim <moreenkim36@gmail.com>
2020-07-02 10:33:06 +0300

    Form enctype multipart

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -1,10 +1,10 @@
 const layout = require('../layout');
 const { getError } = require('../../helpers');
 
-module.exports = () => {
+module.exports = ({ errors }) => {
   return layout({
     content: `
-     <form method = "POST">
+     <form method = "POST" enctype="multipart/form-data">
       <input placeholder="Title" name="title" />
       <input placeholder="Price" name="price" />
       <input type="file" name="image" />

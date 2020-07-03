0395094e767248fbedbff8e301d084783d1bb8b4
moreenkim <moreenkim36@gmail.com>
2020-07-03 12:48:40 +0300

    Add cart Id

--- routes/carts.js
+++ routes/carts.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 //receive post req to add item to cart
+router.post('/cart/products', (req, res) => {
+  console.log(req.body.productId);
+
+  res.send('product added to cart');
+});
 //get req to show items o cart
 
 //post to delete item rfrom caryt

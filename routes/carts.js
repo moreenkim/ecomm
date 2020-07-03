52a25c07bbc20521a2d8364ca49d3640c2abc9fc
moreenkim <moreenkim36@gmail.com>
2020-07-03 14:07:03 +0300

    Add items to the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 const router = express.Router();
 
 //receive post req to add item to cart
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, { items: cart.items });
 
   res.send('product added to cart');
 });

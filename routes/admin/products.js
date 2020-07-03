1c6d94b537f46b41bf0ad32362f5949f2ae58fcd
moreenkim <moreenkim36@gmail.com>
2020-07-03 11:12:40 +0300

    Edit a product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -36,7 +36,7 @@ router.post(
   }
 );
 
-router.get('/admin/products/:id/edit', async (req, res) => {
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
 
   if (!product) {
@@ -45,4 +45,27 @@ router.get('/admin/products/:id/edit', async (req, res) => {
 
   res.send(productsEditTemplate({ product }));
 });
+
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('could not find item');
+    }
+
+    res.redirect('/admin/products');
+  }
+);
+
 module.exports = router;

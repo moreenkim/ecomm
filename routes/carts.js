d211b33d8199199b217399208e40612d2393135b
moreenkim <moreenkim36@gmail.com>
2020-07-03 12:42:07 +0300

    Add shopping cart boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//receive post req to add item to cart
+//get req to show items o cart
+
+//post to delete item rfrom caryt
+
+module.exports = router;
